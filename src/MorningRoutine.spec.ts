5342abd00d9290b7ce1dd6e60f1bc1c332479ef2
Daniel Ramos <danielramosacosta1@gmail.com>
2024-03-08 16:42:44 +0100

    implement displays Do exercise at 06:30

--- src/MorningRoutine.spec.ts
+++ src/MorningRoutine.spec.ts
@@ -4,10 +4,19 @@ import { MorningRoutine } from "./MorningRoutine.js";
 
 describe("MorningRoutine", () => {
   it("displays no exercise when outside schedule", () => {
-    const morningRoutine = new MorningRoutine()
+    const morningRoutine = new MorningRoutine(new Date())
 
     const activity: string = morningRoutine.whatShouldIDoNow()
 
     expect(activity).toBe("No activity")
+  })
+
+  it("displays Do exercise at 06:30", () => {
+    const today = new Date("2024-03-08T05:30:00.000Z");
+    const morningRoutine = new MorningRoutine(today)
+
+    const activity: string = morningRoutine.whatShouldIDoNow()
+
+    expect(activity).toBe("Do exercise")
   })
 })
--- src/MorningRoutine.ts
+++ src/MorningRoutine.ts
@@ -1,5 +1,15 @@
 export class MorningRoutine {
+
+
+  constructor(private today: Date) {
+  }
+
   whatShouldIDoNow() {
+    const hours = this.today.getHours();
+    if (hours === 6) {
+      return "Do exercise"
+    }
+
     return "No activity";
   }
 }
